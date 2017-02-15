cf8746e0e41fdf4c27eba61811b4857e28f1c587
franksowah <efb4@students.calvin.edu>
2017-02-15 13:53:28 -0500

    added homework/optional lab

---
+++ 02modeling/lab02_3.sql
@@ -0,0 +1,68 @@
+-- This command file loads a simple movies database, dropping any existing tables
+-- with the same names, rebuilding the schema and loading the data fresh.
+--
+-- CS 342, Spring, 2015
+-- kvlinden
+
+-- Drop current database
+DROP TABLE Casting;
+DROP TABLE Movie;
+DROP TABLE Performer;
+DROP TABLE Status;
+DROP SEQUENCE movie_sequence;
+
+-- disadvanteges: you hjave to use JOIN to add the table when you want to use it.
+--It might be hard to remember what integer youre using  in the casting table.
+--advantages: you can just change the records in the table instead of changing all the hard coded records.
+CREATE TABLE Status
+	id integer,
+	cast_status varchar(6),
+	PRIMARY KEY (id)
+	);
+	
+-- Create database schema
+CREATE TABLE Movie (
+	id integer,
+	title varchar(70) NOT NULL, 
+	year decimal(4), 
+	score float,
+	votes integer,
+	PRIMARY KEY (id),
+	CHECK (year > 1900)
+	);
+
+CREATE TABLE Performer (
+	id integer,
+	name varchar(35),
+	PRIMARY KEY (id)
+	);
+
+CREATE TABLE Casting (
+	movieId integer,
+	performerId integer,
+	cast_status integer,
+	FOREIGN KEY (movieId) REFERENCES Movie(Id) ON DELETE CASCADE,
+	FOREIGN KEY (performerId) REFERENCES Performer(Id) ON DELETE SET NULL,
+	CHECK (status in ('star', 'costar', 'extra'))
+	);
+
+--create sequence to index movie table
+CREATE SEQUENCE movie_sequence START WITH 1;	
+
+--a. No you do not need an additional sequence for the performer relation primary key values.
+--b. executing time might be a problem using sequences in a DDL file.
+
+-- Load sample data
+INSERT INTO Movie VALUES (movie_sequence.NEXTVAL,'Star Wars',1977,8.9, 2000);
+INSERT INTO Movie VALUES (movie_sequence.NEXTVAL,'Blade Runner',1982,8.6, 1500);
+
+INSERT INTO Performer VALUES (1,'Harrison Ford');
+INSERT INTO Performer VALUES (2,'Rutger Hauer');
+INSERT INTO Performer VALUES (3,'The Mighty Chewbacca');
+INSERT INTO Performer VALUES (4,'Rachael');
+
+INSERT INTO Casting VALUES (1,1,'star');
+INSERT INTO Casting VALUES (1,3,'extra');
+INSERT INTO Casting VALUES (2,1,'star');
+INSERT INTO Casting VALUES (2,2,'costar');
+INSERT INTO Casting VALUES (2,4,'costar');
