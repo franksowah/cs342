a42022eaf63df27548aedff3fff0a5005da9763a
franksowah <efb4@students.calvin.edu>
2017-03-10 21:10:17 -0500

    submitted homeworkl05

---
+++ 05sql/Homework05_1.sql
@@ -0,0 +1,37 @@
+/*
+Emmanuel Boye
+CS 342
+Homework 05
+*/
+
+--1. a.
+SELECT e.employee_ID, e.first_name, e.last_name
+FROM Employees e JOIN Job_History jh on jh.employee_ID = e.employee_ID
+WHERE end_date IS NOT NULL;
+
+
+-- 1. b.
+SELECT e.first_name ||''|| e.last_name AS Employee, e.hire_date AS Employee_Hire_Date, m.first_name ||''|| m.last_name AS Manager, m.hire_date AS Manager_Hire_Date
+FROM Employees e
+JOIN Employees m ON e.manager_ID = m.employee_ID
+AND e.hire_date < m.hire_date
+WHERE EXISTS (SELECT * FROM Job_History jh WHERE e.employee_ID = jh.employee_ID
+AND jh.department_ID = m.department_ID);
+
+
+-- 1. c.
+-- Join Query
+SELECT DISTINCT c.country_name
+FROM Countries c
+JOIN locations l on c.country_ID = l.country_ID
+JOIN Departments d ON d.location_ID = l.location_ID;
+
+-- Nested Query
+SELECT DISTINCT c.country_name
+FROM Countries c, Locations l
+WHERE c.country_ID = l.country_ID
+AND EXISTS (SELECT d.department_ID FROM Departments d
+where d.location_ID = l.location_ID);
+
+/* The join query is better because much faster and has better readability than nested query.
+Oracle is also able to optimize join queries well.
---
+++ 05sql/lab05_3.sql
@@ -0,0 +1,32 @@
+--a.
+SELECT p1.lastName ||', '|| p1.firstName ||' and '|| p2.firstName ||' - '|| h.phoneNumber ||' - '|| h.street AS Family_Entries
+FROM Person p1 JOIN Person p2
+ON p1.householdID = p2.householdID
+AND p1.ID < p2.ID
+JOIN Household h
+ON p2.householdID = h.ID
+AND p2.householdRole <> 'child';
+
+--b.
+SELECT p1.lastName ||', '|| p1.firstName ||' and '|| p2.firstName ||' , '|| P2.lastName ||' - '|| h.phoneNumber ||' - '|| h.street AS Family_Entries
+FROM Person p1 JOIN Person p2
+ON p1.householdID = p2.householdID
+AND p1.ID < p2.ID
+JOIN Household h
+ON p2.householdID = h.ID
+AND p2.householdRole <> 'child';
+
+-- c.
+SELECT p1.lastName ||', '|| p1.firstName ||' and '|| p2.firstName ||' , '|| P2.lastName ||' - '|| h.phoneNumber ||' - '|| h.street AS Family_Entries
+FROM Person p1 JOIN Person p2
+ON p1.householdID = p2.householdID
+AND p1.ID < p2.ID
+JOIN Household h
+ON p2.householdID = h.ID
+AND p2.householdRole <> 'child'
+UNION
+SELECT p.lastName ||', '|| p.firstName ||' - '|| h.phoneNumber ||' - '|| h.street AS Family_Entries
+FROM Person p
+JOIN Household h
+ON p.householdID = h.ID
+WHERE p.householdRole = 'single';
