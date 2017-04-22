abd723de92acd3c2c898de068fdf033827a60a4d
franksowah <efb4@students.calvin.edu>
2017-04-21 22:20:39 -0400

    added procedures.sql

--- project/procedures.sql
+++ project/procedures.sql
@@ -1,19 +1,19 @@
-CREATE OR REPLACE PROCEDURE insertArtiste(artisteIdIn IN integer, nameIn IN artiste.name%type, yearIn In artiste.year%type, actTypeIn IN artiste.actType%type, 
+CREATE OR REPLACE PROCEDURE insertArtiste(artisteIdIn IN integer, nameIn IN artiste.name%type, yearIn In artiste.year%type, actTypeIn IN artiste.actType%type,
 											instrumentIn IN artiste.%type, emailIn IN artiste.email%type, mentorIdIn IN INTEGER, performanceIdIn IN INTEGER)
 							AS
 		counter INTEGER;
-		
+
 BEGIN
-	SELECT COUNT(*) INTO counter FROM Artiste a, Mentor m 
-		WHERE m.ID = a.mentorId;
+	SELECT COUNT(*) INTO counter FROM Artiste a, Mentor m
+		WHERE a.mentorId = m.ID;
 	IF counter > 3 THEN
 		RAISE_APPLICATION_ERROR(-20001, 'Too many Artistes assigned. ' || artsiteIdIn || ' cannot be assigned to mentor')
 	END IF;
-	
+
 	INSERT INTO Artiste( ID, name, year, actType, instrument, email, mentorId, performanceId)
 		VALUES
 		(artisteIdIn, nameIn, yearIn, actTypeIn, instrumentIn, emailIn, mentorIdIn, performanceIdIn);
-	dbms_output.put_line('Artiste ' || artisteIdIn || ' was successfully added to mentor: ' || mentorIdIn);
+	dbms_output.put_line('Artiste ' || artisteIdIn || ' was successfully assigned to mentor: ' || mentorIdIn);
 END;
 /
 
